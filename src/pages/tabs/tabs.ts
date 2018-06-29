33b48953c9d4eb0d00cd049929add528a63b82e4
Carlos Telles <carlos.telles@cwi.com.br>
2018-06-29 17:09:55 -0300

    Ajustes tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import {Component, ViewChild} from '@angular/core';
 import {AboutPage} from '../about/about';
 import {MindsPage} from '../minds/minds';
 import {AddPage} from '../add/add';
+import {Tabs} from 'ionic-angular';
 
 @Component({
   templateUrl: 'tabs.html'
